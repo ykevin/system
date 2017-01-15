9bb3e6c2ffb640fb5063c3e670370247d3426461
ykevin <15018787835@163.com>
2017-01-16 00:17:32 +0800

    add some description

--- README.md
+++ README.md
@@ -4,14 +4,22 @@ System Setting Default
 
 * vim config and vunble plugin
 * tmux
-* ubuntu system install bash
+* ubuntu system install bash and vim plugin auto manager
 * git auto completion
 * add ssh remote password 
 * add rapsberry create img bash
+* add rapsberry ubuntu mate install bash
 
-Install
-=======
+#### Install ubuntu
 > $cd ubuntu_install/  
 $chmod +x install.sh  
 $./install.sh
+
+#### Install raspberry pi3 ubuntu mate
+> $cd rasp-cofig  
+$./Mateinstall.sh
+
+#### make img file for raspberry pi3
+> $cd rasp-config  
+$./buildRaspImg.sh
 
--- rasp-cofig/buildRaspImg.sh
+++ rasp-cofig/buildRaspImg.sh
@@ -1,17 +1,17 @@
 #!/bin/sh
-echo "install package"
+echo "--------install package------------"
 sudo apt-get install -y dosfstools parted kpartx rsync
 
-echo "calculate resize after total size, (root+boot)*1.1"
+echo "------calculate resize after total size, (root+boot)*1.1-----"
 df=`df -P | grep /dev/root | awk '{print $3}'`
 dr=`df -P | grep /dev/mmcblk0p1 | awk '{print $2}'`
 df=`echo $df $dr |awk '{print int(($1+$2)*1.1)}'`
 
-echo "build space img file"
+echo "-------build space img file------"
 sudo dd if=/dev/zero of=raspberrypi.img bs=1K count=$df
 sudo parted raspberrypi.img --script -- mklabel msdos
 
-echo "disk start and end size"
+echo "-----disk start and end size, if other img modfiy about param------"
 start=`sudo fdisk -l /dev/mmcblk0| awk 'NR==9 {print $2}'`
 start=`echo $start's'`
 end=`sudo fdisk -l /dev/mmcblk0| awk 'NR==9 {print $3}'`
@@ -22,28 +22,28 @@ end2=`echo $end2's'`
 sudo parted raspberrypi.img --script -- mkpart primary fat32 $start $end
 sudo parted raspberrypi.img --script -- mkpart primary ext4 $end2 -1
 
-echo "build virtual disk"
+echo "--------build virtual disk-------"
 loopdevice=`sudo losetup -f --show raspberrypi.img`
 device=`sudo kpartx -va $loopdevice | sed -E 's/.*(loop[0-9])p.*/\1/g' | head -1`
 device="/dev/mapper/${device}"
 partBoot="${device}p1"
 partRoot="${device}p2"
 
-echo "format virtual disk "
+echo "----------format virtual disk --------"
 sudo mkfs.vfat $partBoot
 sudo mkfs.ext4 $partRoot
 
-echo "backup boot disk"
+echo "---------backup boot disk-------"
 sudo mount -t vfat $partBoot /media
 sudo cp -rfp /boot/* /media/
 sudo umount /media
 
-echo "backup root disk"
+echo "---------backup root disk--------"
 sudo mount -t ext4 $partRoot /media/
 cd /media
 sudo rsync -aP --exclude="raspberrypi.img" --exclude=/media/* --exclude=/sys/* --exclude=/proc/*  --exclude=/tmp/* / ./
 
-echo "umount all device"
+echo "-----------umount all device---------"
 cd
 sudo umount /media
 sudo kpartx -d $loopdevice
